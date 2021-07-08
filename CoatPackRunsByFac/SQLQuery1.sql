960f571681ed2a466a0a1f8834205527c645d7db
Jed Peterson <Jed.Peterson@bsci.com>
2021-07-08 15:00:50 -0500

    working query

--- CoatPackRunsByFac/SQLQuery1.sql
+++ CoatPackRunsByFac/SQLQuery1.sql
@@ -1,22 +1,17 @@
-select distinct vRPD.RunParamName 
+-- vvv WORKING! vvv
+select vPE.[Desc],
+	vR.runID,
+	vR.Batch,
+	vR.Matl,
+	vR.BeginRunDT,
+	vR.EndRunDT,
+	vRPD.RunParamName,
+	vRPD.RunParamValue
 from CoatPack_MDMS_RPT.dbo.vw_ProcessEntity as vPE
-
-	inner join CoatPack_MDMS_RPT.dbo.vw_Run_ParameterDetail as vRPD
-	on vPE.ProcEntTypeID = vRPD.ProcEntTypeRevID
-
 	inner join CoatPack_MDMS_RPT.dbo.vw_Run as vR
-	on vPE.ProcEntRevID = vR.ProcEntRevID
+		on vPE.ProcEntRevID = vR.ProcEntRevID
+	inner join CoatPack_MDMS_RPT.dbo.vw_Run_ParameterDetail as vRPD
+		on vR.RunID = vRPD.RunID
 
-where vPE.[Desc] = 'PegasusStation01'
-
---('CoatRHSolBatchID',
---	'RackID',
---	'CoatRHRework',
---	'CoatLHEndTime',
---	'CoatRHEndTime',
---	'CoatLHSolBatchID',
---	'CoatLHStartTime',
---	'CureEndTime',
---	'CoatRHStartTime',
---	'CoatLHRework',
---	'CureStartTime')
+where vPE.[Desc] = 'Clean And Coat'
+order by vR.RunID desc
--- CoatPackRunsByFac/SQLQuery2.sql
+++ CoatPackRunsByFac/SQLQuery2.sql
@@ -1,17 +1,54 @@
-select top 100
-	vPE.[Desc],
-	vR.Batch,
-	vR.Matl as Material,
-	vR.BeginRunDT,
-	vR.EndRunDT,
-	vRPD.RunParamName,
-	vRPD.RunParamValue
-from CoatPack_MDMS_RPT.dbo.vw_ProcessEntity as vPE
+-- vvv Debugging; remove in report builder vvv
+declare @pQueryType as varchar(max)
+declare @pNumRuns as int
+declare @pProcessEntity as varchar(max)
+declare @pBatch as varchar(max)
+declare @pStartTime as date
+declare @pEndTime as date
 
-	inner join CoatPack_MDMS_RPT.dbo.vw_Run_ParameterDetail as vRPD
-	on vPE.ProcEntTypeID = vRPD.ProcEntTypeRevID
+set @pQueryType = 'RUNS'
+set @pNumRuns = 10
+set @pProcessEntity = 'PegasusStation01'
+set @pBatch = '27588039'
+set @pStartTime = '2021-07-06'
+set @pEndTime = '2021-07-07'
+-- ^^^ Debugging; remove in report builder ^^^
 
-	inner join CoatPack_MDMS_RPT.dbo.vw_Run as vR
-	on vPE.ProcEntRevID = vR.ProcEntRevID
+if (@pQueryType = 'RUNS')
+	begin
+		select top (@pNumRuns)
+				vPE.[Desc],
+				vR.RunID as [Run ID],
+				vR.Batch,
+				vR.Matl as Material,
+				vR.BeginRunDT as [Start Time],
+				vR.EndRunDT as [End Time],
+				vRPD.RunParamName,
+				vRPD.RunParamValue
+		from CoatPack_MDMS_RPT.dbo.vw_Run as vR
 
-where vPE.[Desc] = 'PegasusStation01'
+			--inner join CoatPack_MDMS_RPT.dbo.vw_ProcessEntity as vPE
+			--	on vR.ProcEntRevID = vPE.ProcEntRevID
+
+			inner join CoatPack_MDMS_RPT.dbo.vw_Run_ParameterDetail as vRPD
+				on vR.runID = vRPD.RunID
+
+			left outer join CoatPack_MDMS_RPT.dbo.vw_ProcessEntity as vPE
+				on vR.ProcEntRevID = vPE.ProcEntRevID
+
+		where vPE.[Desc] = 'PegasusStation02'
+			--and vRPD.RunParamName in --(@pProcessParams)
+			--						('CoatRHSolBatchID',
+			--							'RackID',
+			--							'CoatRHRework',
+			--							'CoatLHEndTime',
+			--							'CoatRHEndTime',
+			--							'CoatLHSolBatchID',
+			--							'CoatLHStartTime',
+			--							'CureEndTime',
+			--							'CoatRHStartTime',
+			--							'CoatLHRework',
+			--							'CureStartTime')
+
+		--order by vR.RunID desc
+	end
